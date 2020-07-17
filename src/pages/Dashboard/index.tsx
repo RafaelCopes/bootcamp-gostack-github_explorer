3482778d3d560a0640bc7540612c139aaee26c1b
RafaeCopes <rafaelcopes16@gmail.com>
2020-07-17 14:59:19 -0300

    getting repositories from api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,126 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+  description: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newRepo, setNewRepo] = useState('');
+
+  async function handleAddRepository(
+    e: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    e.preventDefault();
+
+    const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
-      <Title>Dashboard</Title>
+      <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
-            alt="Rafael Copes"
-          />
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
-            <p>Recursion bitch!</p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
-            alt="Rafael Copes"
-          />
-
-          <div>
-            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
-            <p>Recursion bitch!</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
-            alt="Rafael Copes"
-          />
-
-          <div>
-            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
-            <p>Recursion bitch!</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
-            alt="Rafael Copes"
-          />
-
-          <div>
-            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
-            <p>Recursion bitch!</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
-            alt="Rafael Copes"
-          />
-
-          <div>
-            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
-            <p>Recursion bitch!</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
-            alt="Rafael Copes"
-          />
-
-          <div>
-            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
-            <p>Recursion bitch!</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
-            alt="Rafael Copes"
-          />
-
-          <div>
-            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
-            <p>Recursion bitch!</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
-            alt="Rafael Copes"
-          />
-
-          <div>
-            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
-            <p>Recursion bitch!</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
