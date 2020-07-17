a6b13e334edc9057725fe139b960b722ed4502e5
RafaeCopes <rafaelcopes16@gmail.com>
2020-07-17 00:28:26 -0300

    dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyles from './styles/global';
 import Routes from './routes';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyles />
+  </>
 );
 
 export default App;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,129 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Dashboard</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
+            alt="Rafael Copes"
+          />
+
+          <div>
+            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
+            <p>Recursion bitch!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
+            alt="Rafael Copes"
+          />
+
+          <div>
+            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
+            <p>Recursion bitch!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
+            alt="Rafael Copes"
+          />
+
+          <div>
+            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
+            <p>Recursion bitch!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
+            alt="Rafael Copes"
+          />
+
+          <div>
+            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
+            <p>Recursion bitch!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
+            alt="Rafael Copes"
+          />
+
+          <div>
+            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
+            <p>Recursion bitch!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
+            alt="Rafael Copes"
+          />
+
+          <div>
+            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
+            <p>Recursion bitch!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
+            alt="Rafael Copes"
+          />
+
+          <div>
+            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
+            <p>Recursion bitch!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/43898344?s=460&u=eb74357d7256c90a87aaa720761d2917b006bd6a&v=4"
+            alt="Rafael Copes"
+          />
+
+          <div>
+            <strong>RafaelCopes/bootcamp-gostack-github_explorer</strong>
+            <p>Recursion bitch!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
